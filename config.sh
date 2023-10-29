0be308f488f5cc2177ba7e93317f2c836405ccaf
Anri Gu <anrigu@gl-login3.arc-ts.umich.edu>
2023-10-28 22:31:17 -0400

    Init setup files

---
+++ config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=setup
+#SBATCH --time=00-01:00:00
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=5g
+#SBATCH --account=wellman98
+​
+echo "Job Id listed below:"
+echo $SLURM_JOB_ID 
+​
+module load python cuda
+module load clang 
+module load gcc 
+
+pip install cvxopt
+pip install tensorflow
+pip install matplotlib
+
+cd ~/mfg_research/open_spiel/
+# python3 -m pip install open_spiel
+# python3 -m pip install -r requirements.txt
+./install.sh
+./open_spiel/scripts/build_and_run_tests.sh
---
+++ init_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=setup
+#SBATCH --time=00-01:00:00
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=5g
+#SBATCH --account=wellman98
+​
+echo "Job Id listed below:"
+echo $SLURM_JOB_ID 
+​
+module load python cuda
+module load clang 
+module load gcc 
+
+pip install cvxopt
+pip install tensorflow
+pip install matplotlib
+
+cd ~/mfg_research/open_spiel/
+
+python3 open_spiel/python/examples/example.py --game=breakthrough
